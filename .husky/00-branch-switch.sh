5b7bfa1c787c3763b3a60fce670e197bc23c1e54
Oscar Ludick <oscarludick@gmail.com>
2023-04-17 18:40:26 -0600

    refactor(test-app): pre-commit hook

---
+++ .husky/00-branch-switch.sh
@@ -0,0 +1,42 @@
+BRANCH_NAME=$1
+
+# Check if BRANCH_NAME is empty
+if [ -z "$BRANCH_NAME" ]; then
+  echo "Error: Branch name not specified."
+  exit 1
+fi
+
+git reset HEAD .
+git stash save --keep-index "Stashing added changes"
+
+# Check if branch exists in main repo
+if git show-ref --verify --quiet refs/heads/$BRANCH_NAME; then
+  # If branch exists, switch to it if not already on it
+  if ! git rev-parse --abbrev-ref HEAD | grep -q "^$BRANCH_NAME$"; then
+    git checkout $BRANCH_NAME
+  fi
+else
+  # If branch does not exist, create and checkout it in main repo
+  git branch $BRANCH_NAME
+  git checkout $BRANCH_NAME
+fi
+
+for SUBMODULE in $(git submodule --quiet foreach 'echo $path'); do
+  # Check if there are changes in the submodule
+  if (cd $SUBMODULE && git diff --quiet) && (cd $SUBMODULE && git diff --quiet --staged); then
+    # If no changes in submodule, skip creating and checking out branch
+    continue
+  fi
+
+  if (cd $SUBMODULE && git show-ref --verify --quiet refs/heads/$BRANCH_NAME); then
+    # If branch exists in submodule, switch to it if not already on it
+    if ! git -C $SUBMODULE rev-parse --abbrev-ref HEAD | grep -q "^$BRANCH_NAME$"; then
+      (cd $SUBMODULE && git checkout $BRANCH_NAME)
+    fi
+  else
+    # If branch does not exist in submodule, create and checkout it
+    (cd $SUBMODULE && git branch $BRANCH_NAME && git checkout $BRANCH_NAME)
+  fi
+done
+
+git stash pop
